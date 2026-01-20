aec9637ee1e18279fdc21e93f05c72937af7a6c9
sauravnir <niraulasaurav2@gmail.com>
2026-01-20 21:53:35 +0545

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import {useState, useEffect} from "react"
+import './App.css'
+function App() {
+
+  
+  return (
+    <>
+    <div className="font-extrabold">Hello World</div>  
+    </>
+  )
+}
+
+export default App
